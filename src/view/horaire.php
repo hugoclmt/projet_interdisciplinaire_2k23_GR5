d91b3706dfda435a83fb9916ca5b90e671d0a089
Nicolas Quenon <134261089+Nidoooooo@users.noreply.github.com>
2023-12-18 15:36:24 +0100

    Suite horaire.php + petites modifications

--- src/view/horaire.php
+++ src/view/horaire.php
@@ -1,8 +1,8 @@
 <?php
 require(__DIR__.'/../model/DbModel.class.php');
 ?>
-<h1>Horaire</h1>
-<a href=index.php?page=login.php>Connexion</a> <!-- Lien vers la page de connexion -->
+<section>
+    <h2>Votre horaire</h2>
 <table>
     <?php
     $db=new DbModel('localhost','projet_gr5','root',''); //Connexion à la base de données
@@ -18,12 +18,30 @@ require(__DIR__.'/../model/DbModel.class.php');
     $req->execute();
     $req->setFetchMode(PDO::FETCH_OBJ);
     while ($result=$req->fetch() ) {
+        $timestamp= strtotime($result->date); //On convertit la date en timestamp
+        $formatter = new IntlDateFormatter('fr_FR', IntlDateFormatter::LONG, IntlDateFormatter::NONE); //Permet de traduire la date en français
         echo '<tr>';
-        echo '<td>',$result->date,'</td>';
-        echo '<td>De ',$result->debut,' à ',$result->fin,'</td>';
-        echo '<td>',$result->nbre_heure,'</td>';
-        echo '<td>Congé</td>';
+        echo '<td>',$formatter->format($timestamp),' ','</td>'; //On affiche la date en français
+        echo '<td>De ',$result->debut,' à ',$result->fin,'</td>'; 
+        echo '<td>',$result->nbre_heure,' heures</td>';
+        echo '<td>';
+        if($result->conge==0){ //Si aucune demande n'a été envoyée
+            echo '<a href="">Demander un congé</a>';
+            //TODO Ajouter un controlleur pour la demande de congé
+        }
+        else if($result->conge==1){
+            echo '<a href="">Annuler le congé</a>';
+            
+            if ($result->congeconfirm==0){
+                echo 'La demande a été refusée';
+            }
+            else if ($result->congeconfirm==1){
+                echo 'La demande a été acceptée';
+            }
+        }  
+        echo '</td>';
         echo '</tr>';
     }
     ?>
 </table>
+</section>
