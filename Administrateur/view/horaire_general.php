d9e41acef1c21d5236801104edf692eb33855bc3
Nicolas Quenon <134261089+Nidoooooo@users.noreply.github.com>
2023-12-21 10:07:58 +0100

    Modif de la page horaire général pour pouvoir choisir la semaine

--- Administrateur/view/horaire_general.php
+++ Administrateur/view/horaire_general.php
@@ -1,5 +1,11 @@
 <?php
 $horaire = $controlleur_admin->recuperer_all_horaire();
+//Variables par défaut pour la date et la semaine
+$date_debut = new DateTime();
+$date_fin = new DateTime();
+$date_fin->add(new DateInterval('P7D'));
+$week = $date_debut->format("W");
+$annee = $date_debut->format("Y");
 $nbre_tout_horaire =0;
 if (is_array($horaire) || $horaire instanceof Countable)
 {
@@ -7,23 +13,40 @@ $nbre_tout_horaire = count($horaire);
 }else{
 $msg = "Erreur";
 }
+if (isset($_POST['submit_semaine']))
+{
+    $annee_semaine = explode("-W",$_POST['semaine']); //on recupere l'annee et la semaine
+    $week = $annee_semaine[1];
+    $annee = $annee_semaine[0];
+    $date_debut->setISODate($annee,$week,1);
+    $date_fin->setISODate($annee,$week,7); 
+}
 ?>
 
 <h2>Horaire Generale</h2>
-
-<h3>Semaine X</h3>
+<form method="post">
+    <label for="semaine">Choisir une semaine</label>
+    <input type="week" name="semaine" required>
+    <input type="submit" name="submit_semaine" value="Voir cette semaine">
+</from>
+<?php
+    echo '<h3>Semaine '.$week.', '.$annee.'</h3>';
+?>
 <table>
     <?php
     for ($i = 0;$i<$nbre_tout_horaire;$i++)
     {
+        if ($horaire[$i]['date'] >= $date_debut->format("Y-m-d") && $horaire[$i]['date'] <= $date_fin->format("Y-m-d"))
+        {
         ?>
-        <tr>
-            <td><?php echo $horaire[$i]['date'] ?></td>
-            <td><?php echo $horaire[$i]['debut'] ?> jusque <?php echo $horaire[$i]['fin']?></td>
-            <td><?php echo $horaire[$i]['nbre_heure']?></td>
-            <td>si dmd accepte</td>
-        </tr>
+            <tr>
+                <td><?php echo $horaire[$i]['date'] ?></td>
+                <td><?php echo $horaire[$i]['debut'] ?> jusque <?php echo $horaire[$i]['fin']?></td>
+                <td><?php echo $horaire[$i]['nbre_heure']?></td>
+                <td>si dmd accepte</td>
+            </tr>
         <?php
+        }
     }
     ?>
 
