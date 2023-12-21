a7eb2d80125fa1086e53e60c7d09038b9cda4f75
Nicolas Quenon <134261089+Nidoooooo@users.noreply.github.com>
2023-12-21 16:20:01 +0100

    ajout db 'finale'

--- db/testinterdisciplinaire.sql
+++ db/testinterdisciplinaire.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 5.0.2
+-- version 5.2.0
 -- https://www.phpmyadmin.net/
 --
--- Hôte : 127.0.0.1:3306
--- Généré le : mer. 20 déc. 2023 à 19:42
--- Version du serveur :  5.7.31
--- Version de PHP : 7.4.9
+-- Host: localhost:3306
+-- Generation Time: Dec 21, 2023 at 03:18 PM
+-- Server version: 8.2.0
+-- PHP Version: 8.1.10
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -18,128 +18,164 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Base de données : `testinterdisciplinaire`
+-- Database: `testinterdisciplinaire`
 --
 
 -- --------------------------------------------------------
 
 --
--- Structure de la table `conge`
+-- Table structure for table `conge`
 --
 
-DROP TABLE IF EXISTS `conge`;
-CREATE TABLE IF NOT EXISTS `conge` (
-  `id_conge` int(50) NOT NULL AUTO_INCREMENT,
+CREATE TABLE `conge` (
+  `id_conge` int NOT NULL,
   `date_conge` date DEFAULT NULL,
   `congeconfirm` varchar(50) DEFAULT NULL,
   `justification` varchar(50) DEFAULT NULL,
-  `id_employe` int(50) NOT NULL,
-  PRIMARY KEY (`id_conge`),
-  KEY `id_employe` (`id_employe`)
-) ENGINE=MyISAM AUTO_INCREMENT=6 DEFAULT CHARSET=latin1;
-
---
--- Déchargement des données de la table `conge`
---
-
-INSERT INTO `conge` (`id_conge`, `date_conge`, `congeconfirm`, `justification`, `id_employe`) VALUES
-(2, '2023-12-23', 'Refusé', 'test', 3),
-(3, '2023-12-22', 'Refusé', 'test2', 3),
-(4, '2023-12-23', 'Refusé', 'testbehxhzc', 3),
-(5, '2023-12-30', 'Refusé', 'cdcd', 3);
+  `id_employe` int NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
 
 --
--- Structure de la table `employes`
+-- Table structure for table `employes`
 --
 
-DROP TABLE IF EXISTS `employes`;
-CREATE TABLE IF NOT EXISTS `employes` (
-  `id_employe` int(11) NOT NULL AUTO_INCREMENT,
-  `id_type` int(11) NOT NULL,
+CREATE TABLE `employes` (
+  `id_employe` int NOT NULL,
+  `id_type` int NOT NULL,
   `identifiant` varchar(256) NOT NULL,
   `mdp` varchar(512) NOT NULL,
   `admin` tinyint(1) NOT NULL,
-  `nbre_conges` int(11) NOT NULL DEFAULT '0',
-  PRIMARY KEY (`id_employe`),
-  KEY `fk_employes_type` (`id_type`)
-) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8;
+  `nbre_conges` int NOT NULL DEFAULT '0'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
 
 --
--- Déchargement des données de la table `employes`
+-- Dumping data for table `employes`
 --
 
 INSERT INTO `employes` (`id_employe`, `id_type`, `identifiant`, `mdp`, `admin`, `nbre_conges`) VALUES
-(2, 3, 'quenon.nicolas', 'a94a8fe5ccb19ba61c4c0873d391e987982fbbd3', 1, 14),
-(3, 1, 'root.root', 'cf2e875d70c402e4aaf32ceb64b1fa6f7396af59', 1, 13),
-(6, 4, 'test.test', 'a94a8fe5ccb19ba61c4c0873d391e987982fbbd3', 0, 13);
+(1, 1, 'floriant.alaert', 'a544f49a44e1b75ef70d54123611651dae4cc59d2575a53670c1a6d691ef5dc1', 0, 0),
+(2, 1, 'valentin.barbencon', 'a544f49a44e1b75ef70d54123611651dae4cc59d2575a53670c1a6d691ef5dc1', 0, 0),
+(3, 2, 'arnaud.boucaut', 'a544f49a44e1b75ef70d54123611651dae4cc59d2575a53670c1a6d691ef5dc1', 0, 0),
+(4, 2, 'sebastien.buze', 'a544f49a44e1b75ef70d54123611651dae4cc59d2575a53670c1a6d691ef5dc1', 0, 0),
+(5, 3, 'adrien.debuisson', 'a544f49a44e1b75ef70d54123611651dae4cc59d2575a53670c1a6d691ef5dc1', 0, 0),
+(6, 3, 'françois.debay', 'a544f49a44e1b75ef70d54123611651dae4cc59d2575a53670c1a6d691ef5dc1', 0, 0),
+(7, 4, 'mathieu.agrillo', 'a544f49a44e1b75ef70d54123611651dae4cc59d2575a53670c1a6d691ef5dc1', 0, 0),
+(8, 4, 'thomas.lymans', 'a544f49a44e1b75ef70d54123611651dae4cc59d2575a53670c1a6d691ef5dc1', 0, 0),
+(9, 5, 'jules.dubois', 'a544f49a44e1b75ef70d54123611651dae4cc59d2575a53670c1a6d691ef5dc1', 0, 0),
+(10, 5, 'martin.moreau', 'a544f49a44e1b75ef70d54123611651dae4cc59d2575a53670c1a6d691ef5dc1', 0, 0),
+(11, 6, 'admin', 'a544f49a44e1b75ef70d54123611651dae4cc59d2575a53670c1a6d691ef5dc1', 1, 0);
 
 -- --------------------------------------------------------
 
 --
--- Structure de la table `jour_horaire`
+-- Table structure for table `jour_horaire`
 --
 
-DROP TABLE IF EXISTS `jour_horaire`;
-CREATE TABLE IF NOT EXISTS `jour_horaire` (
-  `id_horaire` int(11) NOT NULL AUTO_INCREMENT,
-  `id_employe` int(11) NOT NULL,
+CREATE TABLE `jour_horaire` (
+  `id_horaire` int NOT NULL,
+  `id_employe` int NOT NULL,
   `date` date NOT NULL,
   `debut` time NOT NULL,
   `fin` time NOT NULL,
-  `nbre_heure` time NOT NULL,
-  PRIMARY KEY (`id_horaire`),
-  KEY `fk_horaire_employe` (`id_employe`)
-) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8;
-
---
--- Déchargement des données de la table `jour_horaire`
---
-
-INSERT INTO `jour_horaire` (`id_horaire`, `id_employe`, `date`, `debut`, `fin`, `nbre_heure`) VALUES
-(1, 2, '2023-12-18', '08:15:00', '17:30:00', '09:15:00'),
-(2, 2, '2023-12-19', '08:00:00', '17:00:00', '09:00:00'),
-(3, 2, '2023-12-20', '08:00:45', '15:00:45', '07:00:00'),
-(4, 3, '2023-12-21', '12:21:49', '19:21:49', '07:21:49'),
-(5, 3, '2023-12-21', '21:04:00', '22:04:00', '00:00:01');
+  `nbre_heure` time NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
 
 -- --------------------------------------------------------
 
 --
--- Structure de la table `type`
+-- Table structure for table `type`
 --
 
-DROP TABLE IF EXISTS `type`;
-CREATE TABLE IF NOT EXISTS `type` (
-  `id_type` int(11) NOT NULL AUTO_INCREMENT,
-  `nom_type` varchar(256) NOT NULL,
-  PRIMARY KEY (`id_type`)
-) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8;
+CREATE TABLE `type` (
+  `id_type` int NOT NULL,
+  `nom_type` varchar(256) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
 
 --
--- Déchargement des données de la table `type`
+-- Dumping data for table `type`
 --
 
 INSERT INTO `type` (`id_type`, `nom_type`) VALUES
-(1, 'ouvrier'),
-(2, 'personnel_nettoyage'),
-(3, 'jardinier'),
-(4, 'maitre-nageur'),
-(5, 'animateur');
+(1, 'animateur'),
+(2, 'jardinier'),
+(3, 'maitre_nageur'),
+(4, 'Nettoyage'),
+(5, 'ouvrier'),
+(6, 'administrateur');
 
 --
--- Contraintes pour les tables déchargées
+-- Indexes for dumped tables
 --
 
 --
--- Contraintes pour la table `employes`
+-- Indexes for table `conge`
+--
+ALTER TABLE `conge`
+  ADD PRIMARY KEY (`id_conge`),
+  ADD KEY `id_employe` (`id_employe`);
+
+--
+-- Indexes for table `employes`
+--
+ALTER TABLE `employes`
+  ADD PRIMARY KEY (`id_employe`),
+  ADD KEY `fk_employes_type` (`id_type`);
+
+--
+-- Indexes for table `jour_horaire`
+--
+ALTER TABLE `jour_horaire`
+  ADD PRIMARY KEY (`id_horaire`),
+  ADD KEY `fk_horaire_employe` (`id_employe`);
+
+--
+-- Indexes for table `type`
+--
+ALTER TABLE `type`
+  ADD PRIMARY KEY (`id_type`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `conge`
+--
+ALTER TABLE `conge`
+  MODIFY `id_conge` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+
+--
+-- AUTO_INCREMENT for table `employes`
+--
+ALTER TABLE `employes`
+  MODIFY `id_employe` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
+
+--
+-- AUTO_INCREMENT for table `jour_horaire`
+--
+ALTER TABLE `jour_horaire`
+  MODIFY `id_horaire` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+
+--
+-- AUTO_INCREMENT for table `type`
+--
+ALTER TABLE `type`
+  MODIFY `id_type` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `employes`
 --
 ALTER TABLE `employes`
   ADD CONSTRAINT `fk_employes_type` FOREIGN KEY (`id_type`) REFERENCES `type` (`id_type`);
 
 --
--- Contraintes pour la table `jour_horaire`
+-- Constraints for table `jour_horaire`
 --
 ALTER TABLE `jour_horaire`
   ADD CONSTRAINT `fk_horaire_employe` FOREIGN KEY (`id_employe`) REFERENCES `employes` (`id_employe`);
